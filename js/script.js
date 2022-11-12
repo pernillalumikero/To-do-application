456ef1bec8bc3e82acfec353fdf39e0d87c75a3e
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2022-11-12 11:59:14 +0100

    objects init

--- js/script.js
+++ js/script.js
@@ -7,112 +7,145 @@ let doneList = document.querySelector("#done-list ul");
 
 let errorMessage = document.querySelector("#error-message");
 
+
+class listItem {
+    constructor(listItem, input, newInput, changebtn, doneBtn, deletebtn) {
+        this.listItem   = listItem;
+        this.input      = input;
+        this.newInput   = newInput;
+        this.changebtn  = changebtn;
+        this.doneBtn    = doneBtn;
+        this.deletebtn  = deletebtn;
+    }
+
+    changeButtonFunction(){
+        //if user tries to add empty string - show error message
+        if (this.input.value == "") {
+            errorMessage.innerText = "Kan inte lägga till uppgift utan innehåll";
+        //unlock input field to let user change input, show save button
+        } else if (this.input.hasAttribute("disabled")) {
+            this.input.removeAttribute("disabled", "");
+            this.changebtn.innerText = "Spara";
+            errorMessage.innerText = "";
+        //reset button to change, lock input field
+        } else {
+            this.changebtn.innerText = "Ändra";
+            this.input.setAttribute("disabled", "");
+            errorMessage.innerText = "";
+            }
+       
+    }
+
+    deleteButtonFunction() {
+        //remove items and related buttons
+        this.listItem.remove();
+        this.changebtn.remove();
+        this.deletebtn.remove();
+        if (this.doneBtn != undefined) {
+            this.doneBtn.remove();
+        }
+    }
+
+    doneButtonFunction() {
+        this.input.setAttribute("disabled", "");
+        this.input.value = this.newInput.value;
+    }
+
+    errorFunction() {
+        //if user tries to add empty string - show error message
+        if (newInput.value == "") {
+            errorMessage.innerText = "Kan inte lägga till uppgift utan innehåll";
+        } //if user inputs string add string to new input field and close field, clear input box for new tasks
+            else {
+            errorMessage.innerText = "";
+            input.value = newInput.value;
+            input.setAttribute("disabled", "");
+            newInput.value = "";
+        }
+    }
+}
+
+
 //add function to addButton on click
 addButton.addEventListener("click", function() {
     //create a li with input in #to-do-list ul - use addInput as value
     let newToDoListItem = document.createElement("li");
     let newToDoInput = document.createElement("input");
-    let input = document.querySelector("#add-container input");
 
     //if user tries to add empty string - show error message
-    if (input.value == "") {
+    if (addInput.value == "") {
         errorMessage.innerText = "Kan inte lägga till uppgift utan innehåll";
     } //if user inputs string add string to new input field and close field, clear input box for new tasks
         else {
         errorMessage.innerText = "";
-        newToDoInput.value = input.value;
+        newToDoInput.value = addInput.value;
         newToDoInput.setAttribute("disabled", "");
         addInput.value = "";
-    
-    //generate change button for newToDo-li
-    let changeBtnToDo = document.createElement("button");
-    changeBtnToDo.innerText = "Ändra";
-    
-    //add change button function on click
-    changeBtnToDo.addEventListener("click", function(e){
-        //if user tries to add empty string - show error message
-        if (newToDoInput.value == "") {
-            errorMessage.innerText = "Kan inte lägga till uppgift utan innehåll";
-        //unlock input field to let user change input, show save button
-        } else if (newToDoInput.hasAttribute("disabled")) {
-            newToDoInput.removeAttribute("disabled", "");
-            e.target.innerText = "Spara";
-            errorMessage.innerText = "";
-        //reset button to change, lock input field
-        } else {
-            e.target.innerText = "Ändra";
-            newToDoInput.setAttribute("disabled", "");
-            errorMessage.innerText = "";
+        
+
+        //generate change button
+        let changeBtnToDo = document.createElement("button");
+        changeBtnToDo.innerText = "Ändra";
+
+        //generate done button for newToDo-li
+        let doneBtnToDo = document.createElement("button");
+        doneBtnToDo.innerText = "Färdig";
+
+        //generate delete button for newToDo-li
+        let deleteBtnToDo = document.createElement("button");
+        deleteBtnToDo.innerText = "Radera"; 
+
+        //initialize object: newToDo
+        let newToDo = new listItem(newToDoListItem, newToDoInput, addInput, changeBtnToDo, doneBtnToDo, deleteBtnToDo);
+
+         //add function to change button on click
+        changeBtnToDo.addEventListener("click", function(){
+            newToDo.changeButtonFunction();
+        });
+
+        //add function to delete button on click
+        deleteBtnToDo.addEventListener ("click", function(){
+            newToDo.deleteButtonFunction();
+        })
+
+        //add function to done button on click
+        doneBtnToDo.addEventListener("click", function(){
+            //create a li with input in #done-list ul - use addInput as value
+            let doneItem = document.createElement("li");
+            let doneInput = document.createElement("input");
+            
+            //generate change button for doneList li
+            let changeBtnDone = document.createElement("button");
+            changeBtnDone.innerText = "Ändra";
+
+            //generate delete button for doneList li
+            let deleteBtnDone = document.createElement("button");
+            deleteBtnDone.innerText = "Radera";
+
+            //initialize object: done
+            let done = new listItem(doneItem, doneInput, newToDoInput, changeBtnDone, undefined, deleteBtnDone);
+
+            done.deleteButtonFunction();
+            done.doneButtonFunction();
+            //doneInput.value = newToDoInput.value;
+            
+            changeBtnDone.addEventListener("click", function(){
+            done.changeButtonFunction();
+            })
+
+            deleteBtnDone.addEventListener ("click", function(){
+                done.deleteButtonFunction();
+            })
+            
+            //add elements to doneList ul and input to li
+            doneList.append(doneItem, changeBtnDone, deleteBtnDone);
+            doneItem.appendChild(doneInput);
+        })
+
+        //add elements to toDoList ul and input to li
+        toDoList.append(newToDoListItem, changeBtnToDo, doneBtnToDo, deleteBtnToDo);
+        newToDoListItem.appendChild(newToDoInput);
         }
-    })
-
-    //generate delete button for newToDo-li
-    let deleteBtnToDo = document.createElement("button");
-    deleteBtnToDo.innerText = "Radera"; 
-
-    //add function to delete button on click
-    deleteBtnToDo.addEventListener ("click", function(){
-        //remove items and related buttons
-        newToDoListItem.remove();
-        changeBtnToDo.remove();
-        doneBtn.remove();
-        deleteBtnToDo.remove();
-    })
-
-    //generate done button for newToDo-li
-    let doneBtn = document.createElement("button");
-    doneBtn.innerText = "Färdig";
-
-    doneBtn.addEventListener("click", function(){
-        newToDoListItem.remove();
-        changeBtnToDo.remove();
-        doneBtn.remove();
-        deleteBtnToDo.remove();
-
-        let doneItem = document.createElement("li");
-        let doneInput = document.createElement("input");
-        doneInput.setAttribute("disabled", "");
-
-        //generate change button for doneList li
-        let changeBtnDone = document.createElement("button");
-        changeBtnDone.innerText = "Ändra";
-
-        changeBtnDone.addEventListener("click", function(e){
-            if (doneInput.value == "") {
-                errorMessage.innerText = "Kan inte lägga till uppgift utan innehåll";
-            } else if (doneInput.hasAttribute("disabled")) {
-                doneInput.removeAttribute("disabled", "");
-                e.target.innerText = "Spara";
-                errorMessage.innerText = "";
-            } else {
-                e.target.innerText = "Ändra";
-                doneInput.setAttribute("disabled", "");
-                errorMessage.innerText = "";
-            }
-        })
-
-        //generate delete button for doneList li
-        let deleteBtnDone = document.createElement("button");
-        deleteBtnDone.innerText = "Radera";
-
-        deleteBtnDone.addEventListener ("click", function(){
-            doneItem.remove();
-            changeBtnDone.remove();
-            deleteBtnDone.remove();
-        })
-        
-        //add toDoList input to doneList input
-        doneInput.value = newToDoInput.value;
-        
-        //add elements to doneList ul and one to li
-        doneList.append(doneItem, changeBtnDone, deleteBtnDone);
-        doneItem.appendChild(doneInput);
-    })
-
-    //add elements to toDoList ul and one to li
-    toDoList.append(newToDoListItem, changeBtnToDo, doneBtn, deleteBtnToDo);
-    newToDoListItem.appendChild(newToDoInput);
-    }
 
 });
 
